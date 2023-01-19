a5d8d55af293168833067032935b61ee68c82263
kyungjin <kyungjin9912@naver.com>
2023-01-19 22:42:44 +0900

    allowed host update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!9a*l0ii%iwe0&v%!m&4o^-r!nnw6no(sqlr+$0j@(w!!*wa=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.35.113', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['54.188.143.105', 'ec2-54-188-143-105.us-west-2.compute.amazonaws.com', 'localhost']
 
 
 # Application definition
